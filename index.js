63268ea9f96994b014d8b27aba53665f19af1de1
“xydzhk” <luck@Luck-Pro.local>
2024-03-15 19:21:28 +0800

    add repository url

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // useGenerateWallets.js
-const generateWallets = require('zoahn');
+const generateWallets = require('xydzhkx');
 
 // 使用第一个项目的功能
-generateWallets(3, 'matthewmed.xlsx');
+generateWallets(3, 'xx.xlsx');
